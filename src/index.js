8dcfbb6e9dca7d8bbeb6e8e4222912a018b606f4
EzequielPires <ezequiel.pires082000@gmail.com>
2022-08-31 19:13:45 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,57 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({
+      error: "Mensagem do erro"
+    })
+  }
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+  if(!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({
+      error: "Mensagem do erro"
+    })
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const {id} = request.params;
+  const user = users.find(user => user.username === username);
+  const todo = user ? user.todos.find(todo => todo.id === id) : false;
+  if (!validate(id)) {
+    return response.status(400).json({
+      error: "Mensagem do erro"
+    })
+  }
+  if(!todo || !user) {
+    return response.status(404).json({
+      error: "Mensagem do erro"
+    })
+  }
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(user => user.id === id );
+  if (!user) {
+    return response.status(404).json({
+      error: "Mensagem do erro"
+    })
+  }
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
